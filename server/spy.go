823b33857c367d1f54582554615d2c43f80f4be0
Scott Davis <skawtus@gmail.com>
2019-04-27 00:13:19 -0700

    testing

--- server/spy.go
+++ server/spy.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func (p *Plugin) spy(username string) {
 
+	user, _ := p.API.GetUserByUsername(username)
+
+	p.API.LogInfo(fmt.Sprintf("%v", user.LastActivityAt))
 }
